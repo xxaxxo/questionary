c9a006c4df3523f634ff55ffcb94b6c3be858c3b
Michael Kumar <michael.kumar@sirma.bg>
2017-12-11 16:20:43 +0200

    Added image conditions for a question

--- src/Question.php
+++ src/Question.php
@@ -8,14 +8,24 @@ class Question
     private $time = false;
     private $condition = array();
 
-    public function setTime($duration)
+    public function setTime($duration, $units = 's')
     {
-        $this->time = (int)$duration;
-    }
-
-    public function getTime()
-    {
-        return $this->time;
+        switch ($units)
+        {
+            case 's':
+                $secondDuration = (int)$duration;
+                break;
+            case 'm':
+                $secondDuration = (int)$duration * 60;
+                break;
+            case 'h':
+                $secondDuration = (int)$duration * 60 * 60;
+                break;
+            default:
+                throw new \Exception('The provided unit for period is not defined - choose between s / m / h');
+                break;
+        }
+        $this->time = (int)$secondDuration;
     }
 
     public function addTextCondition($text)
@@ -28,10 +38,32 @@ class Question
         $this->condition['text'] = $text;
     }
 
+    public function addImageCondition($source, $type = 'link')
+    {
+        $permittedTypes = [
+            'link', 'file'
+        ];
+        if(!in_array($type, $permittedTypes))
+            throw new \Exception('Not permitted type passed - permitted types: '.implode(',',$permittedTypes));
+
+        $image = new \stdClass();
+        $image->type = $type;
+        $image->source = $source;
+        $this->condition['image'] = $image;
+    }
+
+
+    public function getTime()
+    {
+        return $this->time;
+    }
+
     public function getCondition()
     {
         return $this->condition;
     }
+
+
 
 
 }
--- tests/QuestionTest.php
+++ tests/QuestionTest.php
@@ -6,6 +6,7 @@
  */
 
 namespace xXc\Questionary\Tests;
+use Exception;
 use xXc\Questionary\Question;
 
 use PHPUnit\Framework\TestCase;
@@ -34,11 +35,49 @@ class QuestionTest extends TestCase
         $this->assertEquals('30', $this->question->getTime());
     }
 
+    public function testQuestionTimeSetterInMinutes()
+    {
+        $this->question->setTime(1, 'm');
+        $this->assertEquals('60', $this->question->getTime());
+    }
+
+    public function testQuestionTimeSetterException()
+    {
+        $this->expectException(Exception::class);
+        $this->question->setTime(1, 'x');
+    }
+
     public function testAddCondition()
     {
         $this->question->addTextCondition('should we use tests?');
         $this->assertEquals(['text' => 'should we use tests?'], $this->question->getCondition());
     }
+
+
+    public function testAddTextConditionException()
+    {
+        $this->expectException(Exception::class);
+        $this->question->addTextCondition(['exception testing']);
+    }
+
+    public function testAddImageCondition()
+    {
+        $this->question->addImageCondition('http://text.jpg', 'link');
+        $expectedImageObject = new \stdClass();
+        $expectedImageObject->type = 'link';
+        $expectedImageObject->source = 'http://text.jpg';
+        $this->assertEquals(['image' =>
+                                 $expectedImageObject
+        ], $this->question->getCondition());
+    }
+
+    public function testAddImageConditionException()
+    {
+        $this->expectException(Exception::class);
+        $this->question->addImageCondition('image', 'xxx');
+    }
+
+
 
 
 }
