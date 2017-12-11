22b7fc9e84ebeb11cf3faab1bdeee7c172008c19
Michael Kumar <michael.kumar@sirma.bg>
2017-12-11 17:22:09 +0200

    formatted some of the code and added answer setting

--- src/Question.php
+++ src/Question.php
@@ -7,51 +7,75 @@ class Question
 {
     private $time = false;
     private $condition = array();
+    private $validAnswers = array();
+    private $invalidAnswers = array();
+    private $answers = array();
 
-    public function setTime($duration, $units = 's')
+    const TIME_SECONDS = 's';
+    const TIME_MINUTES = 'm';
+    const TIME_HOURS = 'h';
+    const IMAGE_TYPE_LINK = 'link';
+    const IMAGE_TYPE_FILE = 'file';
+    const VALID = 'valid';
+    const INVALID = 'invalid';
+
+    public function setTime($duration, $units = self::TIME_SECONDS)
     {
-        switch ($units)
-        {
-            case 's':
-                $secondDuration = (int)$duration;
-                break;
-            case 'm':
-                $secondDuration = (int)$duration * 60;
-                break;
-            case 'h':
-                $secondDuration = (int)$duration * 60 * 60;
-                break;
-            default:
-                throw new \Exception('The provided unit for period is not defined - choose between s / m / h');
-                break;
-        }
-        $this->time = (int)$secondDuration;
+        $secondDuration = $this->getSecondsDuration($duration, $units);
+        $this->time     = (int)$secondDuration;
     }
 
     public function addTextCondition($text)
     {
-        if(!is_string($text))
-        {
+        if (!is_string($text)) {
             throw new \Exception('The text condition should be a string');
         }
 
         $this->condition['text'] = $text;
     }
 
-    public function addImageCondition($source, $type = 'link')
+    public function addImageCondition($source, $type = self::IMAGE_TYPE_LINK)
     {
         $permittedTypes = [
-            'link', 'file'
+            self::IMAGE_TYPE_LINK,
+            self::IMAGE_TYPE_FILE,
         ];
-        if(!in_array($type, $permittedTypes))
-            throw new \Exception('Not permitted type passed - permitted types: '.implode(',',$permittedTypes));
+        if (!in_array($type, $permittedTypes)) {
+            throw new \Exception('Not permitted type passed - permitted types: '.implode(',', $permittedTypes));
+        }
 
-        $image = new \stdClass();
-        $image->type = $type;
-        $image->source = $source;
+        $image                    = new \stdClass();
+        $image->type              = $type;
+        $image->source            = $source;
         $this->condition['image'] = $image;
     }
 
+    public function addTextAnswer($answer, $valid)
+    {
+        $this->answers[] = $answer;
+        if ($valid == true) {
+            $this->validAnswers[] = $answer;
+        } else {
+            $this->invalidAnswers[] = $answer;
+        }
+    }
+
+    public function getAnswers($filter = null)
+    {
+        if (is_null($filter)) {
+            return $this->answers;
+        }
+
+        if ($filter == self::VALID) {
+            return $this->validAnswers;
+        }
+
+        if ($filter == self::INVALID) {
+            return $this->invalidAnswers;
+        }
+
+        throw new \Exception('Filter for getAnswer accepts only \''.self::VALID.'\' or \''.self::INVALID.'\' params');
+    }
 
     public function getTime()
     {
@@ -63,7 +87,31 @@ class Question
         return $this->condition;
     }
 
+    /**
+     * @param $duration
+     * @param $units
+     * @return int
+     * @throws \Exception
+     */
+    private function getSecondsDuration($duration, $units): int
+    {
+        switch ($units) {
+            case self::TIME_SECONDS:
+                $secondDuration = (int)$duration;
+                break;
+            case self::TIME_MINUTES:
+                $secondDuration = (int)$duration * 60;
+                break;
+            case self::TIME_HOURS:
+                $secondDuration = (int)$duration * 60 * 60;
+                break;
+            default:
+                throw new \Exception('The provided unit for period is not defined - choose between s / m / h');
+                break;
+        }
 
+        return $secondDuration;
+    }
 
 
 }
--- tests/QuestionTest.php
+++ tests/QuestionTest.php
@@ -6,6 +6,7 @@
  */
 
 namespace xXc\Questionary\Tests;
+
 use Exception;
 use xXc\Questionary\Question;
 
@@ -21,7 +22,7 @@ class QuestionTest extends TestCase
     /**
      * @var Question
      */
-    protected  $question;
+    protected $question;
 
     protected function setUp()
     {
@@ -63,12 +64,15 @@ class QuestionTest extends TestCase
     public function testAddImageCondition()
     {
         $this->question->addImageCondition('http://text.jpg', 'link');
-        $expectedImageObject = new \stdClass();
-        $expectedImageObject->type = 'link';
+        $expectedImageObject         = new \stdClass();
+        $expectedImageObject->type   = 'link';
         $expectedImageObject->source = 'http://text.jpg';
-        $this->assertEquals(['image' =>
-                                 $expectedImageObject
-        ], $this->question->getCondition());
+        $this->assertEquals(
+            [
+                'image' => $expectedImageObject,
+            ],
+            $this->question->getCondition()
+        );
     }
 
     public function testAddImageConditionException()
@@ -77,7 +81,54 @@ class QuestionTest extends TestCase
         $this->question->addImageCondition('image', 'xxx');
     }
 
+    public function testAddQuestionAnswer()
+    {
+        $expectedAnswer = 'valid answer';
+        $this->question->addTextAnswer($expectedAnswer, true);
+        $this->assertEquals([$expectedAnswer],
+            $this->question->getAnswers()
+        );
+    }
+
+    public function testAddQuestionAnswers()
+    {
+        $expectedValidAnswer = 'valid answer';
+        $expectedInvalidAnswer = 'valid answer';
+        $this->question->addTextAnswer($expectedValidAnswer, true);
+        $this->question->addTextAnswer($expectedInvalidAnswer, false);
+        $this->assertEquals([$expectedValidAnswer, $expectedInvalidAnswer],
+            $this->question->getAnswers()
+        );
+    }
+
+    public function testGetFilteredInvalidAnswers()
+    {
+        $expectedValidAnswer = 'valid answer';
+        $expectedInvalidAnswer = 'valid answer';
+        $this->question->addTextAnswer($expectedValidAnswer, true);
+        $this->question->addTextAnswer($expectedInvalidAnswer, false);
+        $this->assertEquals([$expectedInvalidAnswer],
+            $this->question->getAnswers('invalid')
+        );
+    }
+
+    public function testGetFilteredValidAnswers()
+    {
+        $expectedValidAnswer = 'valid answer';
+        $expectedInvalidAnswer = 'valid answer';
+        $this->question->addTextAnswer($expectedValidAnswer, true);
+        $this->question->addTextAnswer($expectedInvalidAnswer, false);
+        $this->assertEquals([$expectedValidAnswer],
+            $this->question->getAnswers('valid')
+        );
+    }
 
 
+    public function testGetFilteredValidAnswersException()
+    {
+        $this->expectException(Exception::class);
+        $this->question->getAnswers('xxxx');
+
+    }
 
 }
